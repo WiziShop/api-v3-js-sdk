52ebe3bb43446f6fd78d43dfa032d82110ebbf22
Jean-Baptiste Malatrasi <jb@wizishop.com>
2018-09-20 17:04:58 +0200

    fix typo

--- src/forms/order/order-third-party-order-id-set.form.ts
+++ src/forms/order/order-third-party-order-id-set.form.ts
@@ -2,10 +2,10 @@ import { Observable } from 'rxjs';
 import { WiziApiService } from '../../services/wizi-api.service';
 
 export class OrderThirdPartyOrderIdSetForm {
-  static submit(shopId: number, orderId: number, thirdPartyOrderId: string, thirdPartyOrderForm: string): Observable<any> {
+  static submit(shopId: number, orderId: number, thirdPartyOrderId: string, thirdPartyOrderFrom: string): Observable<any> {
     return WiziApiService.put(`/shops/${shopId}/orders/${orderId}/third-party-order-id`, {
       order_id: thirdPartyOrderId,
-      order_form: thirdPartyOrderForm
+      order_from: thirdPartyOrderFrom
     });
   }
 }
