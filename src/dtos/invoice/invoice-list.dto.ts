855f615d5b0343df5af71509bf133fb028079733
Bruno Carneiro <bruno.carneiro@wizishop.com>
2018-10-05 14:50:22 +0200

    Add InvoiceListGet/IvoiceListDto

---
+++ src/dtos/invoice/invoice-list.dto.ts
@@ -0,0 +1,16 @@
+export interface InvoiceListDto {
+  id: number;
+  ac_id: number;
+  status: string;
+  created_at: Date;
+  paid_at: Date;
+  due_at: Date;
+  type: string;
+  total_price_tax_excluded: string;
+  tax_percent: number;
+  invoiced_by_description: string;
+  invoiced_by_name: string;
+  invoiced_to_address: string;
+  invoiced_to_name: string;
+  invoiced_to_vat_number: number;
+}
