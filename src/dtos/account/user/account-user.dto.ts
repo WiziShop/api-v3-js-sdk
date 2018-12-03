c20c7a333a044827b5636a7cef5812e0c52e08f3
Matthias Octru-Niclas <matthias@wizishop.com>
2018-12-03 17:16:33 +0100

    Manage shop id on Account User

--- src/dtos/account/user/account-user.dto.ts
+++ src/dtos/account/user/account-user.dto.ts
@@ -3,6 +3,7 @@ export interface AccountUserDto {
   email: string;
   first_name: string;
   last_name: string;
+  shop_id: number;
   role_type: 'owner' | 'in_charge' | 'admin' | 'editor' | 'logistician' | 'marketing_manager' | 'designer' | 'custom';
   acl_resources: any;
   created_at: string;
--- src/forms/account/user/account-user-set.form.ts
+++ src/forms/account/user/account-user-set.form.ts
@@ -3,10 +3,10 @@ import { WiziApiService } from '../../../services/wizi-api.service';
 import { AccountUserDto } from '../../../dtos/account/user/account-user.dto';
 
 export class AccountUserSetForm {
-  static submit(acId: number, userId: number, role_type: string): Observable<AccountUserDto> {
+  static submit(acId: number, userId: number, role_type: string, shop_id?: number): Observable<AccountUserDto> {
     return WiziApiService.put<AccountUserDto>(`/accounts/${acId}/users/${userId}`, {
       role_type: role_type,
-      shop_id: null
+      shop_id: shop_id
     });
   }
 }
