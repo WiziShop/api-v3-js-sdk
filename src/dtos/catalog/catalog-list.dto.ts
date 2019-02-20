04f7bf0d28b238f8e74bbd87fecb2237c69548aa
Ludovic Bassilana <ludovic@wizishop.com>
2019-02-20 08:55:00 +0100

    1.2.5 : Adding total result / total pages on catalog-list.dto

--- src/dtos/catalog/catalog-list.dto.ts
+++ src/dtos/catalog/catalog-list.dto.ts
@@ -4,4 +4,6 @@ import { CatalogFacetDto } from './catalog-facet.dto';
 export class CatalogListDto {
   products: Array<CatalogDto>;
   catalog_facet: Array<CatalogFacetDto>;
+  total_results: number;
+  total_pages: number;
 }
