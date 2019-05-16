56bdb8b9ba69d3db9b5d62bd3c763f8455a20ea2
Ludovic Bassilana <ludovic@wizishop.com>
2019-05-16 08:51:46 +0200

    1.3.4 : Remove og_title from post too

--- src/forms/block/home/block-home-post.form.ts
+++ src/forms/block/home/block-home-post.form.ts
@@ -3,17 +3,9 @@ import { WiziApiService } from '../../../services/wizi-api.service';
 import { BlockHomeDto } from '../../../dtos/block/home/block-home.dto';
 
 export class BlockHomePostForm {
-  static submit(
-    shopId: number,
-    title: string,
-    og_title: string,
-    description: string,
-    json_content: string,
-    preview: boolean
-  ): Observable<BlockHomeDto> {
+  static submit(shopId: number, title: string, description: string, json_content: string, preview: boolean): Observable<BlockHomeDto> {
     return WiziApiService.post<BlockHomeDto>(`/shops/${shopId}/block-home`, {
       title: title,
-      og_title: og_title,
       description: description,
       json_content: json_content,
       preview: preview
