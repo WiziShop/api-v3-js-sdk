af7dc0334fd5b20c95a65fb083bd8a42e61bcf40
Alexis Giordan <alexis.giordan@epitech.eu>
2019-06-14 11:41:24 +0200

    Add BusyX Order Create DTO

--- src/forms/busyx/busyx-order-create.form.ts
+++ src/forms/busyx/busyx-order-create.form.ts
@@ -1,9 +1,10 @@
 import { Observable } from 'rxjs';
 import { WiziApiService } from '../../services/wizi-api.service';
+import { BusyXOrderCreateDto } from '../../dtos/busyx/busyx-order-create.dto';
 
 export class BusyXOrderCreateForm {
-  static submit(shop_id: number, order_id: number, payment_id: number, carrier_id: number): Observable<boolean> {
-    return WiziApiService.post<boolean>(`/shops/${shop_id}/busyx/order/create`, {
+  static submit(shop_id: number, order_id: number, payment_id: number, carrier_id: number): Observable<BusyXOrderCreateDto> {
+    return WiziApiService.post<BusyXOrderCreateDto>(`/shops/${shop_id}/busyx/order/create`, {
       order_id: order_id,
       payment_id: payment_id,
       carrier_id: carrier_id
