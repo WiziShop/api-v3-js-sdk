4673ce14127ecda2085ed67a0616f8db5111c31f
Alexis Giordan <alexis.giordan@epitech.eu>
2019-06-26 10:58:49 +0200

    Fix BrandsDistribution Order Create

--- src/forms/brandsdistribution/brandsdistribution-order-create.form.ts
+++ src/forms/brandsdistribution/brandsdistribution-order-create.form.ts
@@ -2,7 +2,10 @@ import { Observable } from 'rxjs';
 import { WiziApiService } from '../../services/wizi-api.service';
 
 export class BrandsDistributionOrderCreateForm {
-  static submit(shopId: number, id: number): Observable<string> {
-    return WiziApiService.post<string>(`/shops/${shopId}/brandsdistributionOrder/${id}`);
+  static submit(shopId: number, id: number, key: string, password: string): Observable<string> {
+    return WiziApiService.post<string>(`/shops/${shopId}/brandsdistributionOrder/${id}`, {
+      key: key,
+      password: password
+    });
   }
 }
