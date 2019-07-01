7b6d996211ec74bd4204e0c73058daf32a124e39
Ludovic Bassilana <ludovic@wizishop.com>
2019-07-01 11:23:10 +0200

    1.5.3 : Remove color_text on emergencybar

--- src/dtos/emergency-bar/emergency-bar.dto.ts
+++ src/dtos/emergency-bar/emergency-bar.dto.ts
@@ -4,7 +4,6 @@ export interface EmergencyBarDto {
   pattern: string;
   text: string;
   button_text: string;
-  color_text: string;
   button_color: string;
   button_color_text: string;
   button_link: string;
