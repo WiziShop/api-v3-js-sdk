4d27732b667c4cfba7b7e1956ec6c44b2a7edc1b
Jean-Baptiste Malatrasi <jb@wizishop.com>
2019-07-01 17:32:45 +0200

    Change route

--- src/forms/brandsdistribution/brandsdistribution-order-create.form.ts
+++ src/forms/brandsdistribution/brandsdistribution-order-create.form.ts
@@ -3,7 +3,7 @@ import { WiziApiService } from '../../services/wizi-api.service';
 
 export class BrandsDistributionOrderCreateForm {
   static submit(shopId: number, id: number, key: string, password: string): Observable<string> {
-    return WiziApiService.post<string>(`/shops/${shopId}/brandsdistributionOrder/${id}`, {
+    return WiziApiService.post<string>(`/shops/${shopId}/brandsdistribution-order/${id}`, {
       key: key,
       password: password
     });
