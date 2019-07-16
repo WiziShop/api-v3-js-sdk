0a0675d053fde7d4756aa4133985811a16f5c9c7
Matthias Octru-Niclas <matthias@wizishop.com>
2019-07-16 15:34:11 +0200

    Add some fields in user DTO

--- src/dtos/user/user.dto.ts
+++ src/dtos/user/user.dto.ts
@@ -3,6 +3,9 @@ export interface UserDto {
   email: string;
   first_name: string;
   last_name: string;
+  phone: string;
+  phone_prefix: string;
+  phone_attempts: string;
   created_at: string;
   updated_at: string;
 }
