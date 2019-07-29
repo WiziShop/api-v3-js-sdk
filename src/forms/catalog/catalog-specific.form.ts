3d2d82064c49694383e1e2006d151aecfbd0844c
Ludovic Bassilana <ludovic@wizishop.com>
2019-07-29 11:22:08 +0200

    1.6.3 : Adding a catalog by ids

--- src/forms/catalog/catalog-specific.form.ts
+++ src/forms/catalog/catalog-specific.form.ts
@@ -2,7 +2,7 @@ import { Observable } from 'rxjs';
 import { WiziApiService } from '../../services/wizi-api.service';
 import { CatalogListDto } from '../../dtos/catalog/catalog-list.dto';
 
-export class CatalogSelectionForm {
+export class CatalogSpecificForm {
   static submit(shopId: number, prodIds: Array<string>, page: number, limit: number): Observable<CatalogListDto> {
     return WiziApiService.get<CatalogListDto>(`/shops/${shopId}/catalog-specific`, {
       prodIds: prodIds,
