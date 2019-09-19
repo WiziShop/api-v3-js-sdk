6f229ee2861f7cd284d111e1ec6f4cc8f7c42bc5
Marvin RAMEIX <marvin@wizishop.com>
2019-09-19 16:48:20 +0200

    1.7.3: Fixing order get and list route mistake

--- src/forms/order/order-get.form.ts
+++ src/forms/order/order-get.form.ts
@@ -3,7 +3,7 @@ import { WiziApiService } from '../../services/wizi-api.service';
 import { OrderDto } from '../../dtos/order/order.dto';
 
 export class OrderGetForm {
-  static submit(shopId: number, orderId: number, publicId = false): Observable<OrderDto> {
-    return WiziApiService.get<OrderDto>(`/shops/${shopId}/orders/${orderId}`, { publicId: publicId, fromStatusCode: 11, toStatusCode: 50 });
+  static submit(shopId: number, orderId: number, isPublicId = false): Observable<OrderDto> {
+    return WiziApiService.get<OrderDto>(`/shops/${shopId}/orders/${orderId}`, { isPublicId: isPublicId });
   }
 }
--- src/forms/order/order-list.form.ts
+++ src/forms/order/order-list.form.ts
@@ -3,18 +3,26 @@ import { WiziApiService } from '../../services/wizi-api.service';
 import { OrderListDto } from '../../dtos/order/order-list.dto';
 
 export class OrderListForm {
-  static submit(idShop: number, page: number, limit: number, sort: string, order: string): Observable<OrderListDto> {
+  static submit(
+    idShop: number,
+    page: number,
+    limit: number,
+    sort: string,
+    order: string,
+    from_status_code: number,
+    to_status_code: number
+  ): Observable<OrderListDto> {
     if (Number(page)) {
       page = Number(page) + 1;
     } else {
       page = 0;
     }
-
-    order = order === 'desc' ? '-' : '';
-    //Etat min payé : 11 - voir _c\Commande
-    const url =
-      '/shops/' + idShop + '/orders?page=' + page + '&limit=' + limit + '&sort=' + order + sort + '&from_status_code=11&to_status_code=50';
-
-    return WiziApiService.get<OrderListDto>(url);
+    return WiziApiService.get<OrderListDto>(`/shops/${idShop}/orders/`, {
+      page: page,
+      limit: limit,
+      sort: order + sort,
+      from_status_code: from_status_code,
+      to_status_code: to_status_code
+    });
   }
 }
