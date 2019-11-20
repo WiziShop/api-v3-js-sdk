b96981cacc41dfd31990577fbe86b09e8e834bf9
Marvin RAMEIX <marvin@wizishop.com>
2019-11-20 11:14:13 +0100

    1.8.4 : adding id_legal in OrderAdvantageValidForm

--- src/forms/order/order-advantage-valid.form.ts
+++ src/forms/order/order-advantage-valid.form.ts
@@ -3,7 +3,7 @@ import { WiziApiService } from '../../services/wizi-api.service';
 import { OrderAdvantageDto } from '../../dtos/order/order-advantage.dto';
 
 export class OrderAdvantageValidForm {
-  static submit(idShop: number, id_sc: number, sendMail: boolean): Observable<OrderAdvantageDto> {
-    return WiziApiService.post<OrderAdvantageDto>(`/shops/${idShop}/store-credits/${id_sc}`, sendMail);
+  static submit(idShop: number, id_sc: number, sendMail: boolean, id_legal: number): Observable<OrderAdvantageDto> {
+    return WiziApiService.post<OrderAdvantageDto>(`/shops/${idShop}/store-credits/${id_sc}`, { sendMail: sendMail, id_legal: id_legal });
   }
 }
