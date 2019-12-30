a45e83e26f26ab6aa042297a7db33e9c13ca078c
Pauline Maes <pauline@wizishop.com>
2019-12-30 16:15:24 +0100

    modified shop close token post to add end_date

--- src/forms/shop/close/shop-close-token-post.form.ts
+++ src/forms/shop/close/shop-close-token-post.form.ts
@@ -2,7 +2,9 @@ import { Observable } from 'rxjs';
 import { WiziApiService } from '../../../services/wizi-api.service';
 
 export class ShopCloseTokenPostForm {
-  static submit(idShop: number): Observable<null> {
-    return WiziApiService.post<null>(`/shops/${idShop}/close-token`);
+  static submit(idShop: number, end_date: string): Observable<null> {
+    return WiziApiService.post<null>(`/shops/${idShop}/close-token`, {
+      end_date: end_date
+    });
   }
 }
